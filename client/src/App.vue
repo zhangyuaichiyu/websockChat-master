746824d85075afce23c632088413922cb26d9607
zhangyuaichiyu <13853326361@163.com>
2021-04-05 11:35:42 +0800

    第一次提交

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app{
+   max-width: 700px;
+}
+</style>
